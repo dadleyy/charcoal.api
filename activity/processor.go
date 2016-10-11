176b2da481b231bc217486c4b37b66ecb244cfa9
Danny Hadley <dadleyy@gmail.com>
2016-10-11 17:05:01 -0400

    restricting concurrency on messages

--- activity/processor.go
+++ activity/processor.go
@@ -12,13 +12,18 @@ const statusPending string = "PENDING"
 const statusApproved string = "APPROVED"
 const statusRejected string = "REJECTED"
 
+type semaphore chan struct{}
+
 type Processor struct {
 	Queue chan Message
 	Logger *log.Logger
 	DBConfig db.Config
 }
 
-func create(message Message, conn *db.Connection, out chan<- error) {
+func create(message Message, conn *db.Connection, out chan<- error, waitlist semaphore) {
+	// wait until someone has released a token from the semaphore
+	waitlist <- struct{}{}
+
 	item := models.Activity{
 		Type: message.Verb,
 		ActorUrl: message.Actor.Url(),
@@ -29,6 +34,8 @@ func create(message Message, conn *db.Connection, out chan<- error) {
 
 	if err := conn.Create(&item).Error; err != nil {
 		out <- err
+		// release our token by receiving from the channel
+		<-waitlist
 		return
 	}
 
@@ -39,15 +46,20 @@ func create(message Message, conn *db.Connection, out chan<- error) {
 
 	if err := conn.Create(&schedule).Error; err != nil {
 		out <- err
+		// release our token by receiving from the channel
+		<-waitlist
 		return
 	}
 
+	// release our token by receiving from the channel
+	<-waitlist
 	out <- nil
 }
 
 func (engine *Processor) Begin() {
 	var deferred sync.WaitGroup
 	makers := make(chan error)
+	waitlist := make(semaphore, 20)
 	conn, err := db.Open(engine.DBConfig)
 
 	if err != nil {
@@ -59,7 +71,7 @@ func (engine *Processor) Begin() {
 	for message := range engine.Queue {
 		deferred.Add(1)
 		engine.Logger.Debugf("spawning creator goroutine for message: %s", message.Verb)
-		go create(message, conn, makers)
+		go create(message, conn, makers, waitlist)
 	}
 
 	go func() {
