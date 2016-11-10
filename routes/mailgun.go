a76e7c23c9a4c6a26f72a80fe1675eeaa00306ac
Danny Hadley <dadleyy@gmail.com>
2016-11-10 17:44:14 -0500

    correcting mailgun route parsing of hook data

--- routes/mailgun.go
+++ routes/mailgun.go
@@ -9,51 +9,23 @@ import "github.com/sizethree/miritos.api/net"
 import "github.com/sizethree/miritos.api/services/mg"
 
 func MailgunUploadHook(runtime *net.RequestRuntime) error {
-	body, err := forms.Parse(runtime.Request)
 	secret := os.Getenv("MAILGUN_WEBHOOK_SECRET")
-
-	if err != nil {
-		return runtime.AddError(err)
-	}
-
 	query := runtime.URL.Query()
 
 	if v, ok := query["secret"]; !ok || len(v) != 1 || v[0] != secret {
 		return runtime.AddError(fmt.Errorf("UNAUTHORIZED"))
 	}
 
-	validator := body.Validator()
-	validator.Require("storage")
-	validator.Require("message")
-
-	// if the validator picked up errors, add them to the request
-	// runtime and then return
-	if validator.HasErrors() == true {
-		for _, m := range validator.Messages() {
-			runtime.AddError(fmt.Errorf(m))
-		}
-
-		return nil
-	}
-
-	info := struct {
-		Storage struct {
-			Url string `json:"url"`
-		}
-	}{}
-
-	if err := body.BindJSON(&info); err != nil {
-		return err
-	}
-
-	runtime.Debugf("found storage url: %s", info.Storage.Url)
-
-	mailgun := mg.Client{os.Getenv("MAILGUN_API_KEY")}
-
-	message, err := mailgun.Retreive(info.Storage.Url)
-
+	body, err := forms.Parse(runtime.Request)
 	if err != nil {
 		return runtime.AddError(err)
+	}
+
+	var message mg.Message
+	message.ContentMap = make(mg.ContentIdMap)
+
+	if err := body.BindJSON(&message); err != nil {
+		return err
 	}
 
 	var processor mg.ActivityProcessor
