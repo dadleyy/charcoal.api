67a1df235fb89f54d09d7d2df7d76cc9368a54f9
Danny Hadley <dadleyy@gmail.com>
2016-12-19 14:51:10 -0500

    adding charset parameter to mysql connection string

--- db/config.go
+++ db/config.go
@@ -2,18 +2,17 @@ package db
 
 import "fmt"
 
-const DSN_STR = "%v:%v@tcp(%v:%v)/%v?parseTime=true"
+const DSN_STR = "%v:%v@tcp(%v:%v)/%v?parseTime=true&charset=utf8mb4,utf8"
 
 type Config struct {
 	Username string
 	Password string
 	Hostname string
 	Database string
-	Port string
-	Debug bool
+	Port     string
+	Debug    bool
 }
 
 func (config *Config) String() string {
 	return fmt.Sprintf(DSN_STR, config.Username, config.Password, config.Hostname, config.Port, config.Database)
 }
-
