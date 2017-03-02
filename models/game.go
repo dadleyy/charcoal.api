b33da0124bab89879b3186701772858f2eb2062a
Danny Hadley <dadleyy@gmail.com>
2017-03-02 16:56:52 -0500

    [CHAPI-21] correcting json serialization typo in game model

--- models/game.go
+++ models/game.go
@@ -8,7 +8,7 @@ type Game struct {
 	Common
 	OwnerID    uint   `json:"owner_id" gorm:"column:owner_id"`
 	Status     string `json:"status"`
-	Population int    `json:"popuplation"`
+	Population int    `json:"population"`
 
 	GameMemberships []GameMembership `json:"-"`
 	Owner           User             `json:"-"`
